ce544f3287357d60ae2bbfb8b10a2fe0c6ca590d
Luan Ibanhez <luanibanhez@gmail.com>
2026-03-27 09:07:46 -0300

    fix: organização de pastas de users, roles, estudantes e auth, e add: rotas para os 4

--- app/Http/Controllers/Api/Estudante/EstudanteController.php
+++ app/Http/Controllers/Api/Estudante/EstudanteController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\Estudante;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class EstudanteController extends Controller
 {
-    
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Estudante\EstudanteController;
+use App\Http\Controllers\Api\Role\RoleController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,5 +28,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::apiResource('/users', UserController::class);
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('roles', RoleController::class);
+    Route::apiResource('estudantes', EstudanteController::class);
 });
